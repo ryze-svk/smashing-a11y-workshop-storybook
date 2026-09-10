0bf5fc7492eec0bca1ee3936ca60799e0b0db311
s.kumpf <s.kumpf@ryze-digital.de>
2026-09-10 14:52:47 +0200

    fix: Fix label color to break the tests

--- src/atoms/Button/IconOnly.stories.tsx
+++ src/atoms/Button/IconOnly.stories.tsx
@@ -6,6 +6,10 @@ const meta = {
   title: 'Broken/Icon Button',
   component: Button,
   parameters: {
+    a11y: {
+      // always a comment, because that's why
+      test: 'todo',
+    }
   }
 } satisfies Meta<typeof Button>;
 
